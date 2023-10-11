bf9720e43df445725bc413363196e90070d08186
Blessedfriday007 <Fridayblessed333@gmail.com>
2023-10-11 12:31:23 +0100

    exercise ..

--- 0x07.py/list.py
+++ 0x07.py/list.py
@@ -7,14 +7,10 @@ fh = open (fname)
 for line in fh:
             # Split the line into words
             line_words = line.split()
-            
-            # Check each word and append it to the list if it's not already there
             for word in line_words:
                 if word not in words:
                     words.append(word)
             words.sort()
-        
-        # Print the sorted words
             for word in words:
              print(word)
 
