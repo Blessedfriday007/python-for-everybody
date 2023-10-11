d77caaead2d9009e260bfe32f8b8973402af01f6
Blessedfriday007 <Fridayblessed333@gmail.com>
2023-10-11 12:21:56 +0100

    exercise 8.5.

--- 0x07.py/list.py
+++ 0x07.py/list.py
@@ -1,1 +1,22 @@
+# use romeo.txt as the file name...
+words = []
+fname = input ("Enter file name: ")
+fh = open (fname)
+
+
+for line in fh:
+            # Split the line into words
+            line_words = line.split()
+            
+            # Check each word and append it to the list if it's not already there
+            for word in line_words:
+                if word not in words:
+                    words.append(word)
+            words.sort()
+        
+        # Print the sorted words
+            for word in words:
+             print(word)
+
+    
 
